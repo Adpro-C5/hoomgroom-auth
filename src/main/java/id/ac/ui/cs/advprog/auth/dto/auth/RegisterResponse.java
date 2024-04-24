4f5b8d2090a6edec50ba4ce8463ee2af6cf63e90
hilaldfzn <hilalfauzan9@gmail.com>
2024-04-24 20:24:09 +0700

    [Feat] Add authentication and profile DTO

---
+++ src/main/java/id/ac/ui/cs/advprog/auth/dto/auth/RegisterResponse.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.auth.dto.auth;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Generated
+public class RegisterResponse {
+    private String message;
+}
