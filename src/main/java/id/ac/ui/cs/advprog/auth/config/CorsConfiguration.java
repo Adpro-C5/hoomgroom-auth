83ec5ef0690e06318dfa6d1447a191057d4750a1
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-05-29 12:11:30 +0700

    Fix Allowed Cors *

--- src/main/java/id/ac/ui/cs/advprog/auth/config/CorsConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/auth/config/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://hoomgroom-frontend-tawny.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
